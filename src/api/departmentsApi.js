8924fea35b7162490da5d1558b0702ba52b37999
AyanChoudhary <darkrider251099@gmail.com>
2019-07-30 22:35:35 +0530

    integrated departmentApi with frontend

--- src/api/departmentsApi.js
+++ src/api/departmentsApi.js
@@ -3,8 +3,6 @@ const axiosInstance = axios.create({
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Origin': 'http://localhost:3000',
-        'Access-Control-Allow-Origin': '*',
     },
     baseURL: 'http://127.0.0.1:8000',
   });
@@ -12,6 +10,7 @@ const axiosInstance = axios.create({
 
 export default function departmentApi() {
     return axiosInstance.get('/api/v1/departments/?format=json').then(function(response) {
-        console.log(response)
+        const res = JSON.parse(response.request.response)
+        return res
     })
 } 
