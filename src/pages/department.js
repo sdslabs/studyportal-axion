92647f7a38a1c4d3b07fab8eac2a35e2ce00d19b
AyanChoudhary <darkrider251099@gmail.com>
2019-08-02 13:57:06 +0530

    made dynamic url routes

---
+++ src/pages/department.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import Header from '../components/header/header'
+import Sidebar from '../components/sidebar'
+import Request from '../components/request'
+import Upload from '../components/upload'
+import ActivityLog from '../components/activityLog'
+import CoursePage from '../components/coursePage'
+import { runInThisContext } from 'vm';
+
+class Department extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: this.props.login,
+            request: false,
+            upload: false,
+            department: '',
+            course: ''
+        }
+
+        this.handleReq = this.handleReq.bind(this);
+        this.handleReqHeader = this.handleReqHeader.bind(this);
+        this.handleUplo = this.handleUplo.bind(this);
+        this.handleUploHeader = this.handleUploHeader.bind(this);
+    }
+
+    componentWillMount() {
+        const department = this.props.match.params.department
+        const course = this.props.match.params.course
+        this.setState({department: department, course: course})
+    }
+
+    handleReqHeader () {
+        this.setState({request: true});
+    }
+
+    handleReq () {
+        this.setState({request: false});
+    }
+
+    handleUploHeader () {
+        this.setState({upload: true});
+    }
+
+    handleUplo () {
+        this.setState({upload: false});
+    }
+
+    render() {
+        return (
+            <div>
+                <Header login={this.state.login} search={this.state.search} handleReqClick={this.handleReqHeader} handleUploClick={this.handleUploHeader} />
+                <Sidebar login={this.state.login} course={this.state.course} department={this.state.department} />
+                <Request request={this.state.request} handleReq={this.handleReq} />
+                <Upload upload={this.state.upload} handleUplo={this.handleUplo} />
+                { this.state.login ? <ActivityLog /> : <CoursePage /> }
+            </div>
+        )
+    }
+}
+
+export default Department
