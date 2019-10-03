22d3e374ddc74d35b3a7f6f0cb05a99ef02e92a6
AyanChoudhary <darkrider251099@gmail.com>
2019-10-03 18:25:01 +0530

    getFilesByType api

--- src/api/filesApi.js
+++ src/api/filesApi.js
@@ -1,8 +1,20 @@
 import { axiosInstance } from 'api/axiosInstance'
 
-export default function getFilesByCourse(id) {
-    return axiosInstance.get(`/api/v1/files/?course=${id}&format=json`).then((response) => {
+function getFilesByCourse(id) {
+    return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=null&format=json`).then((response) => {
         const res = JSON.parse(response.request.response)
         return res
     })
+}
+
+function getFilesByType(id,type) {
+  return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=${type}&format=json`).then((response) => {
+    const res = JSON.parse(response.request.response)
+    return res
+  })
+}
+
+export {
+  getFilesByCourse,
+  getFilesByType
 }
