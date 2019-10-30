f06c8df1f1897cc02a91d8fdd3db84a54c57497c
AyanChoudhary <darkrider251099@gmail.com>
2019-10-30 22:13:26 +0530

    final 404

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import error from 'assets/error.png'
 
 function Error(props) {
   return (
     <div className='error'>
-      Error 404 Not Found
+      <div className='error--image'><img src={error} alt='error' /></div>
+      <div className='error--page'>Page Not Found!</div>
+      <div className='error--sorry'>Sorry we couldn&apos;t find the page you were looking for.</div>
     </div>
   )
 }
