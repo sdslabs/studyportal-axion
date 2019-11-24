ace66b49bde7f928613b20599684e63700272419
AyanChoudhary <darkrider251099@gmail.com>
2019-11-24 19:00:05 +0530

    fix: improve error handling for api requests using reject

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -1,16 +1,24 @@
 import { axiosInstance } from 'api/axiosInstance'
 
 function getCourseByDepartment(id) {
-    return axiosInstance.get(`/api/v1/courses/?department=${id}&course=null&format=json`).then((response) => {
+    return axiosInstance.get(`/api/v1/courses/?department=${id}&course=null&format=json`)
+    .then((response) => {
         const res = JSON.parse(response.request.response)
         return res
+    })
+    .catch((error) => {
+      return Promise.reject(error)
     })
 }
 
 function getCourseInfoByCode(id,code) {
-    return axiosInstance.get(`/api/v1/courses/?department=${id}&course=${code}&format=json`).then((response) => {
+    return axiosInstance.get(`/api/v1/courses/?department=${id}&course=${code}&format=json`)
+    .then((response) => {
       const res = JSON.parse(response.request.response)
       return res[0]
+    })
+    .catch((error) => {
+      return Promise.reject(error)
     })
 }
 
--- src/api/filesApi.js
+++ src/api/filesApi.js
@@ -1,16 +1,24 @@
 import { axiosInstance } from 'api/axiosInstance'
 
 function getFilesByCourse(id) {
-    return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=null&format=json`).then((response) => {
+    return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=null&format=json`)
+    .then((response) => {
         const res = JSON.parse(response.request.response)
         return res
+    })
+    .catch((error) => {
+      return Promise.reject(error)
     })
 }
 
 function getFilesByType(id,type) {
-  return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=${type}&format=json`).then((response) => {
+  return axiosInstance.get(`/api/v1/files/?course=${id}&filetype=${type}&format=json`)
+  .then((response) => {
     const res = JSON.parse(response.request.response)
     return res
+  })
+  .catch((error) => {
+    return Promise.reject(error)
   })
 }
 
