f9156287e216bcd8fd3b8405dc120217d93fac59
AyanChoudhary <darkrider251099@gmail.com>
2019-11-24 19:00:05 +0530

    fix: improve error handling for api requests using reject

--- src/api/departmentApi.js
+++ src/api/departmentApi.js
@@ -1,16 +1,24 @@
 import { axiosInstance } from 'api/axiosInstance'
 
 function getDepartmentsList() {
-    return axiosInstance.get('/api/v1/departments/?format=json').then((response) => {
+    return axiosInstance.get('/api/v1/departments/?format=json')
+    .then((response) => {
         const res = JSON.parse(response.request.response)
         return res
+    })
+    .catch((error) => {
+      return Promise.reject(error)
     })
 }
 
 function getDepartmentInfoByAbbr(department) {
-    return axiosInstance.get(`/api/v1/departments/?department=${department}`).then((response) => {
+    return axiosInstance.get(`/api/v1/departments/?department=${department}`)
+    .then((response) => {
       const res = JSON.parse(response.request.response)
       return res
+    })
+    .catch((error) => {
+      return Promise.reject(error)
     })
 }
 
