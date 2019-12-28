01dd15a575af37cc1dd20af6244cafdccb741e34
AyanChoudhary <darkrider251099@gmail.com>
2019-12-28 16:37:29 +0530

    feat:made status update route for requests

--- src/api/requestApi.js
+++ src/api/requestApi.js
@@ -12,6 +12,18 @@ function requestFiles(user,filetype,title,course) {
   })
 }
 
+function updateRequestStatus(request,status) {
+  return axiosInstance.put('/api/v1/requests', { request,status })
+  .then((response) => {
+    const res = JSON.parse(response.request.response);
+    return res;
+  })
+  .catch((error) => {
+    return Promise.reject(error);
+  })
+}
+
 export {
-  requestFiles
+  requestFiles,
+  updateRequestStatus
 }
