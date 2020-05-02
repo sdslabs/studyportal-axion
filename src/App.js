0af48c13b8a8f5ac0bc1ffd632084348a5fb5fd0
Ishan-002 <58972469+Ishan-002@users.noreply.github.com>
2020-05-02 06:39:02 +0530

    updated getUser()
    modified getUser() to handle all possible cases of login with cookie and token

--- src/App.js
+++ src/App.js
@@ -24,27 +24,84 @@ class App extends Component {
 
   getUser() {
     const token = getCookie('token');
-      if(token) {
-        loginUserWithToken(token).then((res,err) => {
-          if(err) {
-            //TODO handle error
+    const cookie = getCookie('sdslabs');
+    if (token) {
+      loginUserWithToken(token).then((res) => {
+        const user = {
+          login: true,
+          id: res.user.falcon_id,
+          username: res.user.username,
+          email: res.user.email,
+          profile_image: res.user.profile_image,
+          courses: res.courses
+        };
+        this.props.setUser(user);
+        // console.log('The user has logged in with token');
+      })
+        .catch(() => {
+          // console.log('There is some error with the token');
+          if (cookie) {
+            loginUserWithCookie().then((res) => {
+              const user = {
+                login: true,
+                id: res.user.falcon_id,
+                username: res.user.username,
+                email: res.user.email,
+                profile_image: res.user.profile_image,
+                courses: res.courses
+              };
+              this.props.setUser(user);
+              // console.log('The user has logged in with cookie and the invalid token has been replaced');
+            })
+              .catch(() => {
+                const user = {
+                  login: false
+                };
+                this.props.setUser(user);
+                // console.log('The cookie is corrupted');
+                removeCookie('sdslabs');
+                removeCookie('token');
+              });
           }
           else {
             const user = {
-              login: true,
-              id: res.user.falcon_id,
-              username: res.user.username,
-              email: res.user.email,
-              profile_image: res.user.profile_image,
-              courses: res.courses
+              login: false
             };
             this.props.setUser(user);
+            // console.log('There is no cookie present and the token is corrupted');
+            removeCookie('token');
           }
         });
-      }
-      else {
-        loginUserWithCookie();
-      }
+    }
+    else if (cookie) {
+      loginUserWithCookie().then((res) => {
+        const user = {
+          login: true,
+          id: res.user.falcon_id,
+          username: res.user.username,
+          email: res.user.email,
+          profile_image: res.user.profile_image,
+          courses: res.courses
+        };
+        this.props.setUser(user);
+        // console.log('The user did not have the token but is logged in by the cookie and now the token has been created');
+      })
+        .catch(() => {
+          const user = {
+            login: false
+          };
+          this.props.setUser(user);
+          // console.log('The cookie is corrupted (YIKES!)');
+          removeCookie('sdslabs');
+        });
+    }
+    else {
+      const user = {
+        login: false
+      };
+      this.props.setUser(user);
+      // console.log('There is neither cookie nor token present');
+    }
   }
 
   render() {
