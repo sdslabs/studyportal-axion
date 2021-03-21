9c251825fe2f7d492498a0c1df4af533d43b1982
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-03-22 01:18:21 +0530

    feat: add search in logged out sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import arrow from 'assets/back.svg';
 import CourseHandle from './courseHandle';
 import 'styles/main.scss';
 import { Link } from 'react-router-dom';
 import { CLOSE_MODAL } from 'constants/action-types';
+import MiniSearch from 'minisearch';
+import _ from 'lodash';
 
 /**
  * Sidebar component for Studyportal homepage.
@@ -12,14 +14,32 @@ import { CLOSE_MODAL } from 'constants/action-types';
 const Sidebar = () => {
   const dispatch = useDispatch();
   const content = useSelector((state) => state.content);
+  const [courses, setCourses] = useState(content.courses);
 
-  const searchCourse = () => {
-    // TODO search course
+  const miniSearch = new MiniSearch({
+    fields: ['title', 'code'], // fields to index for full-text search
+    storeFields: ['id', 'title', 'code', 'department'],
+    extractField: (document, fieldName) => {
+      // Access nested fields
+      return fieldName.split('.').reduce((doc, key) => doc && doc[key], document);
+    }, // fields to return with search results
+  });
+
+  const searchCourse = (e) => {
+    const searchResult = miniSearch.search(e.target.value);
+    if (_.isEmpty(searchResult)) setCourses(content.courses);
+    else setCourses(searchResult);
   };
 
   const closeModal = () => {
     dispatch({ type: CLOSE_MODAL });
   };
+
+  useEffect(() => {
+    setCourses(content.courses);
+    miniSearch.addAll(content.courses);
+    // eslint-disable-next-line
+  }, [content]);
 
   return (
     <div className="sidebar" onClick={() => closeModal()}>
@@ -33,7 +53,7 @@ const Sidebar = () => {
       </div>
       <div className="sidebar--course-name">
         <div className="sidebar--course-table_logout">
-          {content.courses.map((course) => (
+          {courses.map((course) => (
             <Link
               to={`/departments/${content.activeDepartment.abbr}/courses/${course.code}/`}
               key={course.id}
