cd712d082f86fc7afc543ade8c19a0d3803840f4
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-04-15 12:13:09 +0530

    fix: auth integration

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -25,25 +25,7 @@ export const getUser = (dispatch) => {
       .catch(() => {
         // Token is corrupted
         if (cookie) {
-          loginUserWithCookie()
-            .then((res) => {
-              const user = {
-                login: true,
-                id: res.user.falcon_id,
-                username: res.user.username,
-                email: res.user.email,
-                profile_image: res.user.profile_image,
-                courses: res.courses,
-              };
-              dispatch({ type: SET_USER, payload: user });
-              // Logged in with cookie and the invalid token has been replaced
-            })
-            .catch(() => {
-              dispatch({ type: RESET_APP });
-              removeCookie('sdslabs');
-              removeCookie('token');
-              // The cookie is corrupted, both the token and the cookie have been removed
-            });
+          cookieFlow(dispatch);
         } else {
           dispatch({ type: RESET_APP });
           removeCookie('token');
@@ -51,25 +33,30 @@ export const getUser = (dispatch) => {
         }
       });
   } else if (cookie) {
-    loginUserWithCookie()
-      .then((res) => {
-        const user = {
-          id: res.user.falcon_id,
-          username: res.user.username,
-          email: res.user.email,
-          profile_image: res.user.profile_image,
-          courses: res.courses,
-        };
-        dispatch({ type: SET_USER, payload: user });
-        // The user did not have the token but is logged in by the cookie and the token has been created
-      })
-      .catch(() => {
-        dispatch({ type: RESET_APP });
-        removeCookie('sdslabs');
-        // The cookie is corrupted and removed
-      });
+    cookieFlow(dispatch);
   } else {
     dispatch({ type: RESET_APP });
     // Neither cookie nor token present
   }
 };
+
+function cookieFlow(dispatch) {
+  loginUserWithCookie()
+    .then((res) => {
+      const user = {
+        login: true,
+        id: res.user.falcon_id,
+        username: res.user.username,
+        email: res.user.email,
+        profile_image: res.user.profile_image,
+        courses: res.courses,
+      };
+      dispatch({ type: SET_USER, payload: user });
+      // Logged in with cookie and the invalid token has been replaced
+    })
+    .catch(() => {
+      dispatch({ type: RESET_APP });
+      removeCookie('token');
+      // The cookie is corrupted, both the token and the cookie have been removed
+    });
+}
