945a0645b49226998a97a178cd1036204824115f
Saurabh <saurabh.s@fampay.in>
2021-04-26 10:55:34 +0530

    feat: added reducers and constants for handling navigation

---
+++ src/constants/adminPanelMenu.js
@@ -0,0 +1,9 @@
+export const COURSE_REQUEST_MENU = 'Course Requests';
+export const USER_REQUEST_MENU = 'User Requests';
+export const USER_UPLOADS_MENU = 'User Uploads';
+
+export const ALL_TAB = 'All';
+export const TUT_TAB = 'Tutorials';
+export const BOOKS_TAB = 'Books';
+export const NOTES_TAB = 'Notes';
+export const EXAM_TAB = 'Examination Papers';
--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import userReducer from 'reducers/userReducer';
 import contentReducer from 'reducers/contentReducer';
 import modalReducer from 'reducers/modalReducer';
 import searchReducer from 'reducers/searchReducer';
+import adminPanelReducer from '~/reducers/adminPanelReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   content: contentReducer,
   modal: modalReducer,
   search: searchReducer,
+  adminPanel: adminPanelReducer,
 });
 
 const store = createStore(rootReducer);
