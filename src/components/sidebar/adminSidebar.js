5e083524d41b820ee6fc4c43474e9292927db7e6
Saurabh <saurabh.s@fampay.in>
2021-04-26 14:54:40 +0530

    feat: linked data flow with reducers for side bar

--- src/components/sidebar/adminSidebar.js
+++ src/components/sidebar/adminSidebar.js
@@ -1,94 +1,133 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import arrow from 'assets/left-arrow.svg';
 import { getCourseRequests } from '../../admin/api/courseRequestApi';
 import { getFileRequests } from '../../admin/api/fileRequestApi';
 import { getUploads } from '../../admin/api/uploadsApi';
-
 import { getCookie } from '../../utils/handleCookies';
-
-const tabHeadings = ['Course Requests', 'User Requests', 'User Uploads'];
+import * as constants from 'constants/adminPanelMenu';
+import { useDispatch, useSelector } from 'react-redux';
+import { SwitchMainMenu, SwitchSubMenu, SwitchTab } from 'actions/adminPanelActions';
 
 /*************** Sidebar for admin panel *****************/
-const AdminSidebar = ({ setActiveTab }) => {
-  const [currTab, setCurrTab] = useState(-1);
-  const [menuitems, setmenuitems] = useState([]);
-  const [items, setitems] = useState({});
+const AdminSidebar = () => {
+  const store = useSelector((state) => state.adminPanel);
+  const dispatch = useDispatch();
+  const token = getCookie('token');
 
-  const handleClick = (index) => {
-    const token = getCookie('token');
-    setCurrTab(index);
-    setmenuitems([]);
-    //dispatch action here
-    if (index === 0) {
-      getCourseRequests(token).then((res) => {
-        setmenuitems(res.departments);
-        setitems(res.requests);
-      });
-    } else if (index === 1) {
-      getFileRequests(token).then((res) => {
-        setmenuitems(res.courses);
-        setitems(res.requests);
-      });
-    } else if (index === 2) {
-      getUploads(token).then((res) => {
-        setmenuitems(res.courses);
-        setitems(res.requests);
-      });
-    }
+  console.log(store);
 
-    setActiveTab(index); //temprory switch tab logic
+  const handleBackClick = () => {
+    const payload = {
+      type: '',
+      data: [],
+    };
+    dispatch(SwitchMainMenu(payload));
+    dispatch(SwitchSubMenu(-1));
+    dispatch(SwitchTab(payload));
   };
+
   return (
     <div className="sidebar">
-      <div className="sidebar--course">{currTab === -1 ? 'Admin Panel' : tabHeadings[currTab]}</div>
-      {currTab > -1 && (
-        <div className="sidebar--back" onClick={() => handleClick(-1)}>
+      <div className="sidebar--course">
+        {store.activeMainMenu === '' ? 'Admin Panel' : store.activeMainMenu}
+      </div>
+      {store.activeMainMenu !== '' && (
+        <div className="sidebar--back" onClick={handleBackClick}>
           <img src={arrow} alt="arrow" /> <span className="back">Back</span>
         </div>
       )}
-      {currTab === -1 ? (
-        <MainMenu currTab={currTab} handleClick={handleClick} />
-      ) : (
-        <SubMenu menuitems={menuitems} currTab={currTab} />
-      )}
+      <MainMenu store={store} dispatch={dispatch} token={token} />
+      <SubMenu store={store} dispatch={dispatch} />
     </div>
   );
 };
 
-const MainMenu = ({ currTab, handleClick }) => {
+const MainMenu = ({ store, dispatch, token }) => {
+  const menuItems = [
+    constants.COURSE_REQUEST_MENU,
+    constants.USER_REQUEST_MENU,
+    constants.USER_UPLOADS_MENU,
+  ];
+
+  const handleDispatch = (type, subMenu, tableData) => {
+    dispatch(
+      SwitchMainMenu({
+        type: type,
+        data: subMenu,
+      }),
+    );
+    dispatch(
+      SwitchTab({
+        type: constants.ALL_TAB,
+        data: tableData,
+      }),
+    );
+    dispatch(SwitchSubMenu(0));
+  };
+
+  const handleClick = (type) => {
+    switch (type) {
+      case menuItems[0]:
+        getCourseRequests(token).then((res) => {
+          handleDispatch(type, res.departments, res.requests);
+        });
+        break;
+
+      case menuItems[1]:
+        getFileRequests(token).then((res) => {
+          handleDispatch(type, res.courses, res.requests);
+        });
+        break;
+
+      case menuItems[2]:
+        getUploads(token).then((res) => {
+          handleDispatch(type, res.courses, res.requests);
+        });
+        break;
+
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className="sidebar--course-name">
-      {tabHeadings.map((heading, key) => (
+    <div className={`${store.activeMainMenu === '' ? '' : 'hidden'} sidebar--course-name`}>
+      {menuItems.map((type, key) => (
         <div
-          className={currTab === key ? 'coursehandle_active' : 'coursehandle'}
+          className={store.activeMainMenu === type ? 'coursehandle_active' : 'coursehandle'}
           key={key}
-          onClick={() => handleClick(key)}
+          onClick={() => handleClick(type)}
         >
-          <span className="coursehandle--heading">{heading}</span>
+          <span className="coursehandle--heading">{type}</span>
         </div>
       ))}
     </div>
   );
 };
 
-const SubMenu = ({ menuitems, currTab }) => {
+const SubMenu = ({ store, dispatch }) => {
   return (
-    <div className="sidebar--course-name">
-      {menuitems.length !== 0 ? (
-        menuitems.map((request, key) => (
-          <div className={'coursehandle'} key={key}>
-            {currTab === 0 ? (
-              <span className="coursehandle--heading">{request.title}</span>
-            ) : (
-              <span className="coursehandle--heading">
-                {request.title} | {request.code}
-              </span>
-            )}
+    <div className={`${store.activeSubMenu > -1 ? '' : 'hidden'} sidebar--course-name`}>
+      {store.subMenuData.length !== 0 ? (
+        store.subMenuData.map((item, key) => (
+          <div
+            className={store.activeSubMenu === key ? 'coursehandle_active' : 'coursehandle'}
+            key={key}
+            onClick={() => dispatch(SwitchSubMenu(key))}
+          >
+            <span className="coursehandle--heading">
+              {item.title}
+              {item.abbreviation && ` | ${item.abbreviation}`}
+            </span>
           </div>
         ))
       ) : (
-        <div>Nothing to show</div>
+        <div className="coursehandle">
+          <span className="coursehandle--heading" style={{ opacity: 0.5 }}>
+            Nothing to show Here
+          </span>
+        </div>
       )}
     </div>
   );
