d6d98975070b9e104768a320cf234466468f3cda
mhk19 <gmahak1@gmail.com>
2021-05-31 22:05:59 +0530

    [WIP]add support to download uploads

--- src/components/adminTable/userUploadsTable.js
+++ src/components/adminTable/userUploadsTable.js
@@ -9,6 +9,11 @@ const UserUploadsTable = () => {
   const activeData = store.tableData[Object.keys(store.tableData)[store.activeSubMenu]];
   const token = getCookie('token');
 
+  const downloadFile = (url) => {
+    const link = `https://drive.google.com/a/iitr.ac.in/uc?id=${url}&export=download`;
+    window.open(link, '_blank');
+  };
+
   if (activeData?.length === 0) return null;
 
   return (
@@ -31,7 +36,12 @@ const UserUploadsTable = () => {
           </div>
           <div className="admin-table--secondary-row">
             <div className="row-item">
-              <TableIconButton type="download" />
+              <TableIconButton
+                type="download"
+                handleClick={() => {
+                  downloadFile(item.driveid);
+                }}
+              />
             </div>
             <div className="row-item">
               <TableIconButton type="preview" />
