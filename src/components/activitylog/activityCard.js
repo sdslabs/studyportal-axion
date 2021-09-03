9e06293ca43ed3b8f41883e84f619808e22f9141
Shreya Bhagat <shreyabhagat2002@gmail.com>
2021-09-03 20:45:33 +0530

    fix: changes requested for date ordinal

--- src/components/activitylog/activityCard.js
+++ src/components/activitylog/activityCard.js
@@ -43,14 +43,15 @@ const ActivityCard = (props) => {
     if (date) {
       let datePart = date.match(/\d+/g);
       let month = getMonth(`${datePart[1]}`);
-      let dateString = `${datePart[2]}th ${month}, ${datePart[0]}`;
+      let ordinal = 'th';
       if (datePart[2] === '01' || datePart[2] === '21' || datePart[2] === '31') {
-        dateString = `${datePart[2]}st ${month}, ${datePart[0]}`;
+        ordinal = 'st';
       } else if (datePart[2] === '02' || datePart[2] === '22') {
-        dateString = `${datePart[2]}nd ${month}, ${datePart[0]}`;
+        ordinal = 'nd';
       } else if (datePart[2] === '03' || datePart[2] === '23') {
-        dateString = `${datePart[2]}rd ${month}, ${datePart[0]}`;
+        ordinal = 'rd';
       }
+      let dateString = `${datePart[2]}${ordinal} ${month}, ${datePart[0]}`;
       return dateString;
     }
   };
