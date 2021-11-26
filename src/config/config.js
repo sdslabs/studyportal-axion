6174693f59899d286b7f0185e95d714bc0da72d3
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-11-26 19:41:06 +0530

    chore: repo cleanup and README corrections
    fix typo in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,17 @@
-export const CONFIG = {
-  nexusRoot: 'http://localhost:8005/api/v1',
-  arceusRoot: 'http://arceus.sdslabs.local',
-  studyRoot: 'http://studyportal.sdslabs.local',
-  mediaRoot: 'http://localhost:8005',
-};
+const DEVELOPMENT = true;
+
+export const CONFIG = DEVELOPMENT
+  ? {
+      nexusRoot: 'http://localhost:8005/api/v1',
+      arceusRoot: 'http://arceus.sdslabs.local',
+      studyRoot: 'http://studyportal.sdslabs.local',
+      mediaRoot: 'http://localhost:8005',
+      wsRoot: 'ws://localhost:8005',
+    }
+  : {
+      nexusRoot: 'https://nexus.sdslabs.co/api/v1',
+      arceusRoot: 'https://accounts.sdslabs.co',
+      studyRoot: 'https://study.sdslabs.co',
+      mediaRoot: 'https://nexus.sdslabs.co',
+      wsRoot: 'ws://nexus.sdslabs.co',
+    };
